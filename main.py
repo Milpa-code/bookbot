64b0ffbb8d17fcec78cd637e3928f8e6d09e100b
Milpa <miwwachu2@gmail.com>
2025-05-27 15:33:21 +0000

    1

--- main.py
+++ main.py
@@ -1,1 +1,6 @@
-print("greetings boots")
+def get_book_text():
+    with open("/home/milpa/git/bookbot/books/frankenstein.txt") as f:
+        return f
+def main():
+    f.read(get_book_text)
+print(main)
