9a51cc86932111ade590449f7efc58324b8f04e1
Milpa <miwwachu2@gmail.com>
2025-05-27 17:29:50 +0000

    4

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 def get_book_text():
-    f = open('frankenstein.txt', 'w', encoding="utf-8")
-    return f
-def main():
-    file_contents = []
-    file_contents = f.read(get_book_text)
-    return file_contents
+    with open('frankenstein.txt', 'w', encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data)
