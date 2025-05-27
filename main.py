713178eaa2fe8a00c9d955b6dbb3fadec20eaf94
Milpa <miwwachu2@gmail.com>
2025-05-27 17:34:46 +0000

    7

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 def get_book_text():
-    with open("/books/frankenstein.txt", 'w', encoding="utf-8") as f:
+    with open("/home/milpa/git/bookbot/books/frankenstein.txt", 'w', encoding="utf-8") as f:
        print(f.read())
