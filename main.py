24e477cb786e6add31f7f0739133b3c6120888f1
Milpa <miwwachu2@gmail.com>
2025-05-27 19:29:27 +0000

    3

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 def get_book_text(filepath):
-    with open("filepath") as f:
+    with open(filepath) as f:
         read_text = f.read()
     return read_text
 def main():
     result = get_book_text("books/frankenstein.txt")
     print(result)
+main()
